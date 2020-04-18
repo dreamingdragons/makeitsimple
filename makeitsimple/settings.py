cf92bfc177e5df9029c9e035b4c7491a90ee6141
Stoic <dreamingdragons7@gmail.com>
2020-04-18 15:08:59 +0530

    make it simple v1

--- makeitsimple/settings.py
+++ makeitsimple/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = security.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1', 'localhost', 'make-it-simple.herokuapp.com','www.make-it-simple.herokuapp.com',
+    'sparklers.cf','www.sparklers.cf'
+]
 
 
 # Application definition
