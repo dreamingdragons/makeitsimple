cf7cf752418d2fa675887918b0672b6b2a309388
Stoic <dreamingdragons7@gmail.com>
2020-04-18 15:56:58 +0530

    make it simple v1

--- makeitsimple/security.py
+++ makeitsimple/security.py
@@ -1,5 +1,10 @@
 import os
 
 # FOR PRODUCTION USE
+# class security():
+# 	SECRET_KEY = os.environ['SECRET_KEY']
+
+
+# SECURITY ISSUE - for develepment use only
 class security():
-	SECRET_KEY = os.environ['SECRET_KEY']
+	SECRET_KEY = '55ij0_6bt^x^f@w_*h0nx02aa^lz8hrl8gcpdgxkk6_ye+_a9d'
--- makeitsimple/settings.py
+++ makeitsimple/settings.py
@@ -124,10 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 #django heroku app simpleisbetterthancomplex
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
