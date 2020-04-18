6d890f7278cca1c7c09aca85bd292a51816dab5a
Stoic <dreamingdragons7@gmail.com>
2020-04-18 15:38:50 +0530

    make it simple v1

--- makeitsimple/settings.py
+++ makeitsimple/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = security.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '127.0.0.1', 'localhost', 'make-it-simple.herokuapp.com','www.make-it-simple.herokuapp.com','sparklers.cf','www.sparklers.cf',
+    '127.0.0.1', 'localhost', 'make-it-simple.herokuapp.com','www.make-it-simple.herokuapp.com',
+    'sparklers.cf','www.sparklers.cf'
 ]
 
 
@@ -77,6 +78,13 @@ WSGI_APPLICATION = 'makeitsimple.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
